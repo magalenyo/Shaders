e130e18471bf80cbf785b4c216c02d33c1ef3b32
magalenyo <magalenyo7@gmail.com>
2020-12-14 00:24:13 +0100

    Initial commit

---
+++ phongPrueba_SimplePS.glsl
@@ -0,0 +1,39 @@
+#version 330
+
+in vec3 surface_normal;
+in vec3 surface_position;
+
+uniform vec3 light_dir;
+uniform vec3 view_pos;
+uniform vec3 ambient_color;
+uniform float Ks;
+uniform float Kd;
+uniform float shininess;
+uniform vec3 light_color;
+uniform vec3 diffuse_color;
+
+out vec4 outColor;
+
+void main() {
+	vec3 nlight_dir = normalize(light_dir);
+	float NL = max(dot(normalize(surface_normal), nlight_dir), 0.0);		// diffuse
+	
+	vec3 ambient = ambient_color * diffuse_color;
+	if (NL > 0) {	// if in front of light
+		vec3 R = reflect(nlight_dir, normalize(surface_normal));
+		vec3 V = normalize(view_pos - surface_position);
+		float VR = max(dot(V, R), 0.0);
+		vec3 specular = Ks * light_color * pow(VR, shininess);		// specular
+		vec3 diffuse = Kd * diffuse_color * light_color * NL;
+		
+		outColor = normalize(vec4(ambient + 
+					    diffuse +
+					    specular, 1.0));
+
+	}
+	else{	// is black
+		outColor = normalize(vec4(ambient, 1.0));
+	}
+	
+	 
+}
---
+++ phongPrueba_SimpleVS.glsl
@@ -0,0 +1,19 @@
+#version 330
+layout (location = 0) in vec3 vertex_position;
+layout (location = 1) in vec3 vertex_normal;
+layout (location = 1) in vec2 vertex_uv0;
+
+uniform mat4 proj;
+uniform mat4 view;
+uniform mat4 model;
+
+out vec3 surface_normal;
+out vec3 surface_position;
+out vec2 uv;
+
+void main() {
+   gl_Position 		= proj*view*model*vec4(vertex_position, 1.0);
+   surface_position = (model*vec4(vertex_position, 1.0)).xyz;
+   surface_normal	= transpose(inverse(mat3(model)))*vertex_normal;
+   uv = vertex_uv0;
+}
